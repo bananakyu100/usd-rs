1adccedc855288b159ca7399b780b5cc3190a58f
Luke Titley <luke@titley.com>
2020-11-24 00:22:37 +0100

    Playing with bind file. Not able to produce the header file that I want yet

--- usd-rs/tools/bind/tftoken.cpp
+++ usd-rs/tools/bind/tftoken.cpp
@@ -2,7 +2,7 @@
 
 #define CPPMM_IGNORE __attribute__((annotate("cppmm:ignore")))
 #define CPPMM_RENAME(x) __attribute__((annotate("cppmm:rename:" #x)))
-#define CPPMM_OPAQUEBYTES __attribute__((annotate("cppmm:opaquebytes")))
+#define CPPMM_OPAQUEPTR __attribute__((annotate("cppmm:opaqueptr")))
 
 namespace cppmm_bind {
 PXR_NAMESPACE_OPEN_SCOPE
@@ -10,7 +10,8 @@ PXR_NAMESPACE_OPEN_SCOPE
 namespace pxr = PXR_INTERNAL_NS;
 
 class TfToken {
-    //constexpr TfToken() noexcept {};
+public:
+    TfToken() CPPMM_RENAME(ctor);
 } CPPMM_OPAQUEBYTES;
 
 PXR_NAMESPACE_CLOSE_SCOPE

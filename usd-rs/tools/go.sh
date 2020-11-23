4495cca76fc846f26729d294a078a299de09f56a
Luke Titley <luke@titley.com>
2020-11-23 22:59:29 +0100

    Added a util to call cppmm. This will be the starting point for experimenting with cppmm

---
+++ usd-rs/tools/go.sh
@@ -0,0 +1,8 @@
+export PATH=/Volumes/src/cppmm/build
+
+cppmm ../bind -u                                    \
+    -l ../../target/debug/deps/libusd.dylib         \
+    -o usd-c                                        \
+    --                                              \
+    -I../thirdparty/docs/include                    \
+    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/lib/clang/11.0.0/include
